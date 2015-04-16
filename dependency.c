d6e5672fca152a3e0b0bcf058c6ebe730e0140d7
lester.crespo <lester.crespo.es@gmail.com>
2015-04-16 14:34:11 +0100

    Documented a simple task dependency algorithm to be implement. it is going to be use in kernel module to deferred modules initialization base on dependencies

---
+++ dependency.c
@@ -0,0 +1,30 @@
+/*
+ * dependency.c
+ *
+ * It implements a task scheduler with dependencies.
+ * Every task can depends only from another one, not multiple dependencies is allowed
+ *
+ * A task has this members.
+ * status - block by dependency, waiting to run
+ * depends - the task that depends on
+ *
+ * When a task is complete all dependencies are unblocked, one of them is execute in the same thread
+ * if there is no more dependencies to unblock then a waiting one is pick from the list.
+ *
+ * every running task is move to the end of the list
+ * when list size reach zero then the running thread finish.
+ *
+ * if there is no any task to do then the thread must wait in a waiting queue until waiting task !=0
+ * ones all task are done ( list size ==0) then waiting task get the value -1 to wakeup all waiting threads
+ * and release all thread because list is empty.
+ * spin lock have to be use to access the list
+ *
+ *
+ *
+ *  Created on: 16 Apr 2015
+ *      Author: lester.crespo
+ */
+
+
+
+
